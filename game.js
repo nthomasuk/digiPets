db23ab36b3b75e9b09c061c7da021ce4afbf72e1
ntcreationsuk <66379881+ntcreationsuk@users.noreply.github.com>
2022-07-01 12:39:49 +0100

    removes unneeded npm modules

--- game.js
+++ game.js
@@ -1,8 +1,6 @@
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
-import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
 
 import { Dog } from './dog.js'
